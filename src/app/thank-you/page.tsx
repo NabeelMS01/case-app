37f707025c52200231974f345f73962b385c7004
NabeelMS01 <nabeelmampallil123@gmail.com>
2024-06-23 13:15:08 +0530

    fx

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,13 @@
+import { Suspense } from "react";
+import ThankYou from "./Thankyou";
+
+const Page = () => {
+    
+  return (
+    <Suspense>
+      <ThankYou />
+    </Suspense>
+  );
+};
+
+export default Page;
