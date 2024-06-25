361748acebbc47b0051d6a09f12be3aba6eb174d
NabeelMS01 <nabeelmampallil123@gmail.com>
2024-06-25 20:30:16 +0530

    fix

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -62,7 +62,7 @@ export async function POST(req: Request) {
         },
       });
       await resend.emails.send({
-        from: "CaseCobra <nabeelms097@gmail.com>",
+        from: "onboarding@resend.dev",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order",
         react: OrderRecievedEmail({
